035e5dbfdcd7de61b0ef0bacddf920064a6a7d02
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-27 12:31:16 +0530

    Convert Binary Number in a Linked List to Integer
    This was my final submission
    Runtime: 4 ms, faster than 43.15% of C++ online submissions
    Memory Usage: 8.3 MB, less than 62.46% of C++ online submissions

--- linked-list/problem-1290.cpp
+++ linked-list/problem-1290.cpp
@@ -9,25 +9,15 @@
  * };
  */
  
- // SUBMISSION 1
+ // FINAL SUBMISSION
  
 class Solution {
 public:
     unsigned int getDecimalValue(ListNode* head) {
-        ListNode* traverse = head;
-        unsigned int num=0, inter=0;
-        int y=0, arr[30], i=0;
-        while(traverse!=NULL){
-            arr[i]=traverse->val;
-            i++;
-            traverse = traverse->next;
-        }
-        int len = i;
-        for(int i=len-1; i>=0; i--)
-        {
-            inter = pow(2,y);
-            num += arr[i]*inter;
-            y++;
+        unsigned int num = head->val;
+        while(head->next != NULL){
+            num = num*2 + head->next->val;
+            head = head->next;
         }
         return num; 
     }
